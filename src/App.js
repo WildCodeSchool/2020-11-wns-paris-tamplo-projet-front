85b7cda07c8f806a758d75a22988ca875d0c990c
Emily Déat <emily.deat@gmail.com>
2020-11-17 12:11:12 +0100

    Add router for teacher and student

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 import React from 'react'
+import { Switch, Route, Link } from 'react-router-dom'
 
 import Student from './components/Student'
 import Teacher from './components/Teacher'
 
-import './App.css'
+import { Nav, ButtonNav } from "./styles/element";
 
 const App = () => {
   return (
-    <div className="App">
-      <Student />
-      <Teacher />
+    <div className="app-container">
+      <Switch>
+        <Route exact path='/'>
+          <Nav>
+            <ButtonNav>
+              <Link to='/student'>Student</Link>
+            </ButtonNav>
+            <ButtonNav>
+              <Link to='/teacher'>Teacher</Link>
+            </ButtonNav>
+          </Nav>
+        </Route>
+        <Route path="/student" component={Student} />
+        <Route path="/teacher" component={Teacher} />
+      </Switch>
     </div>
   )
 }
