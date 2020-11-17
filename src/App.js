172b80bce4495f7f493dfa91f08381cdab2d7975
Emily Déat <emily.deat@gmail.com>
2020-11-17 11:48:54 +0100

    Init project

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import Student from './components/Student'
+import Teacher from './components/Teacher'
+
+import './App.css'
+
+const App = () => {
+  return (
+    <div className="App">
+      <Student />
+      <Teacher />
+    </div>
+  )
+}
+
+export default App
