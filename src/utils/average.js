9cc984c42cc8e486c945aa7751380dd212904a20
SebG-prog <sebastien-gruet@orange.fr>
2020-11-18 10:42:08 +0100

    Implemented Mood average functions in Mood table

---
+++ src/utils/average.js
@@ -0,0 +1,16 @@
+export const averageValueOfArray = (arr, id, keyId, keyValue) => {
+  const list = getFilteredList(arr, id, keyId, keyValue)
+  if (!list.length) return 0
+  return getAverageOfList(list).toFixed(1)
+}
+
+const getFilteredList = (arr, id, keyId, keyValue) => {
+  return arr.reduce(
+    (total, { [keyId]: idValue, [keyValue]: value }) =>
+      idValue === id ? [...total, value] : total,
+    []
+  )
+}
+
+const getAverageOfList = (arr) =>
+  arr.reduce((total, value) => total + value, 0) / arr.length
