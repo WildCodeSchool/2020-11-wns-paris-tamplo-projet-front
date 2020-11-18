a5b8f64bc7368692fb3451b1b2a541ae84327f28
Emily Déat <emily.deat@gmail.com>
2020-11-18 11:16:53 +0100

    Add form for student

--- src/components/Student.js
+++ src/components/Student.js
@@ -1,7 +1,35 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Student = () => {
-  return <div>Student</div>
+  const [students, setStudents] = useState(null)
+
+  const getStudents = async () => {
+    const result = await axios.get('http://localhost:8080/students')
+    setStudents(result.data)
+  }
+
+  useEffect(() => {
+    getStudents()
+  }, [])
+
+  if (students === null) {
+    return <p>Loading...</p>
+  }
+  return (
+    <div>
+      <label htmlFor="student-select">Qui es tu ?</label>
+
+      <select name="student" id="student-select">
+        {students.map((student) => (
+          <option value={student.firstname}>
+            {student.firstname} {student.lastname}
+          </option>
+        ))}
+      </select>
+      <button>Ok</button>
+    </div>
+  )
 }
 
 export default Student
