2a4a26ac446b9a094213d9d9c12142d18f3437e4
SebG-prog <sebastien-gruet@orange.fr>
2021-05-06 21:44:30 +0200

    Setting var env config for all setup (prod, staging, dev)

--- deploy_develop_branch.sh
+++ deploy_develop_branch.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 git fetch origin && git reset --hard origin/develop && git clean -f -d
-docker-compose -f docker-compose-develop.yml up --build -d
+docker-compose -f docker-compose.yml up --build -d
