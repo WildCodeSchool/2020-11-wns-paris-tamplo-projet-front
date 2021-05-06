bd19e35fd57c4c7e2e7e64ab331a5130b62e4f23
Emily Déat <emily.deat@gmail.com>
2021-05-06 10:38:58 +0200

    Add Hourra in classroom

--- src/components/Teacher/TeacherClassroom.tsx
+++ src/components/Teacher/TeacherClassroom.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const TeacherClassroom = (): JSX.Element => {
   return (
     <div className="teacherHome-container">
-      Je suis la page classe du Teacher
+      Je suis la page classe du Teacher ! Hourra !
     </div>
   )
 }
